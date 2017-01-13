8a7dd0a458133dfea2d777bdc9bb32b44425ab66
Stephanie Spears <stephanie.spears1@gmail.com>
2017-01-13 13:53:26 -0800

    add javascript elements

---
+++ js/scripts.js
@@ -0,0 +1,11 @@
+$("h1").click(function() {
+  alert("This is a header.");
+});
+
+$("div.my-menu").click(function() {
+  alert("Redirecting to Epicodus Site.");
+});
+
+$("img").click(function() {
+  alert("This is an image.");
+});
