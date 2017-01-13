b55a842ab4bf20f5f3fa4f29945373732dc54fc7
Stephanie Spears <stephanie.spears1@gmail.com>
2017-01-13 15:21:19 -0800

    add form

--- js/scripts.js
+++ js/scripts.js
@@ -7,5 +7,30 @@ $("div.my-menu").click(function() {
 });
 
 $("div.take-survey").click(function() {
-  prompt("Front-end or Back-end?");
+  alert("Directing to track selector");
+});
+
+
+
+$("my-survey.html").ready(function() {
+  $("#blanks form").submit(function(event) {
+    var question1Input = $("input#question1").val();
+    var question2Input = $("input#question2").val();
+    var question3Input = $("input#question3").val();
+    var question4Input = $("input#question4").val();
+    var question5Input = $("input#question5").val();
+
+    $(".question1").text(question1Input);
+    $(".quesiton2").text(question2Input);
+    $(".question3").text(question3Input);
+    $(".question4").text(question4Input);
+    $(".question5").text(question5Input);
+    // $(".noun").text(nounInput);
+
+    $("#answer1").show();
+    $("#answer2").show();
+    $("#answer3").show();
+
+    event.preventDefault();
+  });
 });
