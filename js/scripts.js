7431479fb110db097f29d6c1e7c74ec074f95a75
Stephanie Spears <stephanie.spears1@gmail.com>
2017-01-17 15:38:07 -0800

    remove jquery path, format causing errors

--- js/scripts.js
+++ js/scripts.js
@@ -1,31 +1,31 @@
 $("h1").click(function() {
   alert("This is a header.");
 });
-//
-// $("div.my-menu").click(function() {
-//   alert("Redirecting to Epicodus Site.");
-// });
-//
-// $("div.take-survey").click(function() {
-//   alert("Directing to track selector");
-// });
 
-
-$("my-survey.html").ready(function() {
-var question1 = $("input:radio[name=question1]:checked").val();
-
-    if (question1 === "checked") {
-      $("#answer1").show();
-    } else {
-      $("question2").show();
-    }
-
-    if (answer2==="yes"){
-      $("#answer2").show();
-    } else {
-    $("#answer3").show();
-  }
-
-    event.preventDefault();
-
+$("div.my-menu").click(function() {
+  alert("Redirecting to Epicodus Site.");
 });
+
+$("div.take-survey").click(function() {
+  alert("Directing to track selector");
+});
+
+//
+// $("my-survey.html").ready(function() {
+// var question1 = $("input:radio[name=question1]:checked").val();
+//
+//     if (question1 === "checked") {
+//       $("#answer1").show();
+//     } else {
+//       $("question2").show();
+//     }
+//
+//     if (answer1 === "checked") {
+//       $("#answer2").show();
+//     } else {
+//     $("#answer3").show();
+//   }
+//
+//     event.preventDefault();
+//
+// });
